5e8c51cfd714c188131cf315ef2a0151ee99c124
eduardo <eduardo.rodriguez.info@gmail.com>
2023-07-28 16:05:58 -0400

    add validation entity task

--- src/main/java/com/company/api/component/response/ApiResponse.java
+++ src/main/java/com/company/api/component/response/ApiResponse.java
@@ -1,11 +1,9 @@
 package com.company.api.component.response;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApiResponse {
-
     public <T> Response createCorrectResponse() {
         return Response.builder()
                 .ok(true)
